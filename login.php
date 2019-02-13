f8381465ce4669fe98295488973904b1138f7883
Unknown <oparram@unbosque.edu.co>
2019-02-13 00:57:25 -0500

    Login

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin - Login</title>
+  <title>Login</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -19,39 +19,27 @@
 
 </head>
 
-<body class="bg-dark">
+<body background="img/champions.jpg" >
 
   <div class="container">
-    <div class="card card-login mx-auto mt-5">
-      <div class="card-header">Login</div>
-      <div class="card-body">
-        <form>
-          <div class="form-group">
-            <div class="form-label-group">
-              <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="required" autofocus="autofocus">
-              <label for="inputEmail">Email address</label>
+      <div class="card card-login mx-auto mt-5">
+        <div class="card-header">Login</div>
+        <div class="card-body">
+          <form action="" method="post">
+            <div class="form-group">
+              <div class="form-label-group">
+                <input name="user" type="text" id="usuario" class="form-control" placeholder="Usuario" required="required" >
+                <label for="usuario">Usuario</label>
+              </div>
             </div>
-          </div>
-          <div class="form-group">
-            <div class="form-label-group">
-              <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="required">
-              <label for="inputPassword">Password</label>
+            <div class="form-group">
+              <div class="form-label-group">
+                <input name="password" type="password" id="clave" class="form-control" placeholder="Contraseña" required="required">
+                <label for="clave">Contraseña</label>
+              </div>
             </div>
-          </div>
-          <div class="form-group">
-            <div class="checkbox">
-              <label>
-                <input type="checkbox" value="remember-me">
-                Remember Password
-              </label>
-            </div>
-          </div>
           <a class="btn btn-primary btn-block" href="index.html">Login</a>
         </form>
-        <div class="text-center">
-          <a class="d-block small mt-3" href="register.html">Register an Account</a>
-          <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
-        </div>
       </div>
     </div>
   </div>
@@ -66,3 +54,90 @@
 </body>
 
 </html>
+
+<?php
+
+    session_start();
+
+
+
+
+
+    function verificar_login($user,$password,&$result)  // COMPARAMOS SI EXISTE EL RESULTADO
+    {
+
+    	$conn = mysqli_connect("localhost","root", "", "badbunny");
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+        $sql = "SELECT * FROM administrador WHERE usuario_admin='$user' and pass_admin='$password'";
+
+
+
+		$rec = $conn->query($sql);
+        $count = 0;
+        while($row = $rec->fetch_assoc())
+        {
+            $count++;
+            $result = $row;
+        }
+        if($count == 1)
+        {
+
+            $_SESSION['cod_admin'] = $row['cod_admin'];
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+
+
+
+    if(!isset($_SESSION['cod_admin']))
+    {
+        if(isset($_POST['login']))
+        {
+          if(empty($_POST['user'])) {
+            echo "<p class='error'> * El campo usuario no puede estar vacío </p>";
+          }
+            if(empty($_POST['password'])) {
+            echo "<div class='error'> * El campo constraseña no puede estar vacío </div>";
+              }
+            if(verificar_login($_POST['user'],$_POST['password'],$result) == 1)
+            {
+                $_SESSION['cod_admin'] = $result->cod_admin;
+
+
+                   header("location:inicioAdmin.php"); // Acá es el login correcto, y reenviamos
+            }
+            else
+            {
+                echo '<span class="error">Usuario o contraseña incorrecto, intente de nuevo.</span>'; // mensaje de error
+            }
+        }
+
+        ?>
+
+<?php
+      }
+      else
+      {
+          echo 'Su usuario ingreso correctamente.';
+          echo '<a href="logout.php">Logout</a>';
+      }
+      // eso es logout cunado cerramos sesion vemos como es codigo
+    ?>
+
+    <style>
+    body{
+    background: url(img/champions.jpg) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    }
+    </style>
